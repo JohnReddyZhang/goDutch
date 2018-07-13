34a98cfbcc6aacc6cc4c7da17fb3d44989d14f9e
JohnReddyZhang <zhzhang135@me.com>
2018-07-12 23:09:45 -0500

    reformatted form elements, got the idea of post / element parsing. Learning to implement a mongodb. TODO: Reformat form from restaurant-join. TODO: RESTful routing. TODO: database support. Postgres? Mongodb? (thinking about going with mongodb TODO: actual restaurant information page. (part of RESTful route)

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -8,6 +8,7 @@ router.get('/restaurant-list/join', function (req, res) {
 
 router.post('/restaurant-list', function (req, res) {
     console.log("Received a post request.");
+    console.log(req.body.restaurant);
     res.redirect('/restaurant-list/success');
 });
 
